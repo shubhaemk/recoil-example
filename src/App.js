59af6428e6220c64ce30f3cb5eb84be474fe8e42
Shubham Kamath <dev.shubhaem@gmail.com>
2020-05-16 01:38:56 +0530

    Container of components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import AddItem from './components/addItem';
+import FilterItem from './components/filterItem';
+import DisplayList from './components/displayList';
+
 import './App.css';
 
 function App() {
+  /* 
+  * The other way
+  * 
+  const [stateList, setList] = useRecoilState(list);
+  const addItem = () => {
+    setList([...stateList, { id: Math.random(), text: inputValue }]);
+  }
+ */
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="action">
+        <AddItem />
+        <FilterItem />
+      </div>
+      <DisplayList />
     </div>
   );
 }
